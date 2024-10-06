90d58e99439250390e25f2a5c992e0b342a8636f
mochedayan <hortizlaime@gmail.com>
2024-10-06 17:42:00 -0500

    descripción

--- Semana_2.py
+++ Semana_2.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 #titulo de la aplicación
 st.title("Introducción a variables y tipos de datos en Python")
+
+#creando descripción inicial
+st.write("Python es un lenghaje de programación dinámico donde ...")
